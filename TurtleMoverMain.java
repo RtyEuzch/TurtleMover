ecd084660fc96172786a1f31ad5db644bdfd61d0
RtyEuzch <137963453+RtyEuzch@users.noreply.github.com>
2023-12-31 19:28:12 -0600

    Changes

--- TurtleMoverMain.java
+++ TurtleMoverMain.java
@@ -1,6 +1,24 @@
+/* Import Statements */
+import java.awt.*;
+import javax.swing.*;
 
+/**
+ */
 public class TurtleMoverMain {
     public static void main(String[] args) {
-        
+        JFrame frame = new JFrame("Bouncing Block");
+        TurtleComponent comp = new TurtleComponent();
+        frame.setSize(800, 800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(comp);
+    }
+}
+
+
+class TurtleComponent extends JComponent {
+    @Override 
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+
     }
 }
